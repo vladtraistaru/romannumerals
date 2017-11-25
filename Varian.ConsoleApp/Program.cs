5b8b04633685baff3d319da4d666f1fe6ef2cfe4
Vlad Traistaru <vlad.traistaru@gmail.com>
2017-11-25 18:16:01 +0200

    adding console app

--- Varian.ConsoleApp/Program.cs
+++ Varian.ConsoleApp/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Varian.RomanNumerals.DI;
+using Varian.StringTransform.DI;
+using Varian.StringTransform.Services.Interfaces;
 
 namespace Varian.ConsoleApp
 {
@@ -10,6 +14,26 @@ namespace Varian.ConsoleApp
     {
         static void Main(string[] args)
         {
-        }
+            var kernel = new StandardKernel(new RomanNumeralModule(), new StringTransformModule());
+            var transform = kernel.Get<IStringTransform>();
+
+            for(; ; )
+            {
+                Console.WriteLine("Give input string...");
+                var input = Console.ReadLine();
+                Console.WriteLine("Output string is :");
+                Console.WriteLine(transform.Transform(input));
+
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to try again or Esc to exit.");
+                var key = Console.ReadKey();
+                if(key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("By!");
+            Thread.Sleep(500);
+        }    
     }
 }
